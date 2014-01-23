01573ed321e3f01263caea348a51e109a35d03b0
frikache <novikov_a@rambler.ru>
2014-01-23 18:40:58 +0300

    Todo Project
    Не очень удачная попытка реализовать приложение todo  с помощью паттерна
    MVVM

---
+++ TodoProject/TodoProject/ModelView.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TodoProject
+{
+    class ModelView
+    {
+        public int CountTodo;
+        public List<StackPanel> ListStackPanels = new List<StackPanel>();
+        public List<Todo> ListTodos = new List<Todo>();
+
+        
+
+        public void CreateTodo()
+        {
+            StackPanel newStackPanel = new StackPanel();
+            ListTodos.Add(new Todo(CountTodo, TextBox1.Text));
+
+            Label newLabel = new Label();
+            newLabel.Name = "label" + CountTodo;
+            newLabel.Width = 800;
+            newLabel.Content = ListTodos[CountTodo].GetTodoStr;
+            newStackPanel.Children.Add(newLabel);
+          
+            CheckBox newCheckBox = new CheckBox();
+            newCheckBox.Name = "checkbox" + CountTodo;
+            newCheckBox.Content = "Выполнено?";
+            newCheckBox.IsChecked = false;
+            newCheckBox.IsThreeState = false;
+            newCheckBox.Checked += new RoutedEventHandler(ch1_check);
+            newStackPanel.Children.Add(newCheckBox);
+            
+            CountTodo++;
+        }
+
+        public void ch1_check(object sender, RoutedEventArgs e)
+        {
+            RemoveTodo();
+        }
+
+        public void RemoveTodo()
+        {
+            int intCycle = 0;
+            while (intCycle < CountTodo)
+            {
+              /*  if (ListCheckBoxs[intCycle].IsChecked.GetValueOrDefault())
+                {
+                    ListTodos[intCycle].GetDone = true;
+                    wrap1.Children.RemoveAt(2 * intCycle);
+                    wrap1.Children.RemoveAt(2 * intCycle);
+                    ListTodos.Remove(ListTodos[intCycle]);
+                    ListCheckBoxs.Remove(ListCheckBoxs[intCycle]);
+                    CountTodo--;
+                }
+               */ intCycle++;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ TodoProject/TodoProject/Todo.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TodoProject
+{
+    public class Todo : INotifyPropertyChanged
+    {
+        private int Number;
+        private string TodoStr;
+        private bool Done;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Todo()
+        {
+        }
+
+        public Todo(int num, string el)
+        {
+            Number = num;
+            TodoStr = el;
+            Done = false;
+        }
+
+        public override string ToString()
+        {
+            return TodoStr.ToString();
+        }
+
+        public int GetNumber
+        {
+            get { return Number; }
+            set
+            {
+                Number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+
+        public string GetTodoStr
+        {
+            get { return TodoStr; }
+            set
+            {
+                TodoStr = value;
+                OnPropertyChanged("TodoStr");
+            }
+        }
+
+        public bool GetDone
+        {
+            get { return Done; }
+            set
+            {
+                Done = value;
+                OnPropertyChanged("Done");
+            }
+        }
+
+        protected void OnPropertyChanged(string info)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        private void cmdDeleteTodo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+        
+    }
+    public class ListTodos : ObservableCollection<Todo>
+    {
+        public ListTodos() : base()
+    {
+      
+    }
+
+
+    }
+}
